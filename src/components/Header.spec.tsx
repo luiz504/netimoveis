36e579dd0fac093344099851470d32ab37b0d5dc
luiz bueno <luiz504@hotmail.com>
2024-05-09 20:53:56 -0300

    chore: config vitest

---
+++ src/components/Header.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import { Header } from './Header'
+
+describe('Component: Header', () => {
+  it('should render correctly', () => {
+    render(<Header />)
+    expect(screen.getByText('logo')).toBeInTheDocument()
+  })
+})
