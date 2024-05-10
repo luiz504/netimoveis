5626a97423a71af71157e61d12395a580e8c19ad
luiz bueno <luiz504@hotmail.com>
2024-05-10 14:46:29 -0300

    chore: config svgr/tailwind-prettier plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,59 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      maxWidth: {
+        '312': '78rem',
+        '172': '43rem',
+      },
+
+      colors: {
+        'off-white': '#EDF1F4',
+        'gray-100': '#F3F3F3',
+        'gray-200': '#E1E5E9',
+        'gray-300': '#D9D9D9',
+        'gray-400': '#D0D5D8',
+        'gray-450': '#C0C7Ca',
+        'gray-500': '#A1A7AA',
+        'gray-600': '#808587',
+        'gray-650': '#848484',
+        'gray-700': '#555555',
+        'gray-800': '#4E5254',
+        'gray-900': '#3A3A3A',
+        'gray-950': '#393B3D',
+        'gray-1000': '#161616',
+        'purple-500': '#8224CB',
+        'purple-700': '#652D91',
+        'orange-400': '#F36C21',
+        'orange-500': '#ED672B',
+        'orange-600': '#E95808',
+        'blue-100': '#063D7E',
+      },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        slideLeftAndFade: {
+          from: { opacity: '0', transform: 'translateX(2px)' },
+          to: { opacity: '1', transform: 'translateX(0)' },
+        },
+        slideUpAndFade: {
+          from: { opacity: '0', transform: 'translateY(2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        slideRightAndFade: {
+          from: { opacity: '0', transform: 'translateX(-2px)' },
+          to: { opacity: '1', transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideLeftAndFade:
+          'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideRightAndFade:
+          'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
     },
   },
